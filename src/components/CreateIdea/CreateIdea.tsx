ead1f1dd1db943f3c7a5007e5cec0c1593542376
Josh D <josh.date@techguilds.com>
2022-08-22 13:16:49 -0400

    small changes to create page

--- src/components/CreateIdea/CreateIdea.tsx
+++ src/components/CreateIdea/CreateIdea.tsx
@@ -1,6 +1,9 @@
 import clsx from "clsx";
 import { useForm, SubmitHandler } from "react-hook-form";
 import styles from "./CreateIdea.module.css";
+import backArrow from "../../assets/icons/back-arrow.png";
+import circleButton from "../../assets/icons/plus-circle-button.png";
+import plusIcon from "../../assets/icons/plus-icon.png";
 
 enum CategoryEnum {
   Healthcare = "Healthcare",
@@ -41,69 +44,48 @@ const CreateIdea = () => {
   const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
   return (
-    <div className="flex flex-col items-center h-full">
-      <div className={styles["title"]}>Create An Inspring Idea!</div>
-      <form onSubmit={handleSubmit(onSubmit)} className={styles["container"]}>
-        <div className="flex flex-col w-[322px] items-center mb-5 md:w-[400px] lg:w-[500px]">
-          <input
-            {...register("idea")}
-            className={styles["input"]}
-            placeholder="Enter Your Life Changing Idea!"
-          />
-        </div>
-        <div className="flex flex-col w-[322px] items-center mb-5 md:w-[400px] lg:w-[500px]">
-          <textarea
-            {...register("description")}
-            className={clsx(styles["input"], styles["input-description"])}
-            placeholder="Enter Your Life Changing Idea!"
-          />
-        </div>
-        <div className="flex flex-col items-center w-[322px] mt-5 md:w-[400px] lg:w-[500px]">
-          <select
-            {...register("category")}
-            className={styles["input"]}
-            value="Select An Industry"
-          >
-            <option value="Select An Industry" disabled hidden>
-              Select an Industry
-            </option>
-            <option value="Healthcare">Healthcare</option>
-            <option value="Technology">Technology</option>
-            <option value="Construction">Construction</option>
-            <option value="Retail">Retail</option>
-            <option value="NondurableManufacturing">
-              Nondurable Manufacturing
-            </option>
-            <option value="Telecommunication">Telecommunication</option>
-            <option value="Automobile">Automobile</option>
-            <option value="Manufacturing">Manufacturing</option>
-            <option value="OilandGas">Oil and Gas</option>
-            <option value="FoodIndustry">Food Industry</option>
-            <option value="RealEstate">Real Estate</option>
-            <option value="RetailTrade">Retail Trade</option>
-            <option value="Web 3">Web 3</option>
-            <option value="Utilities">Utilities</option>
-            <option value="WholesaleTrade">Wholesale Trade</option>
-            <option value="WasteManagement">Waste Management</option>
-            <option value="Transportation">Transportation</option>
-            <option value="LifeandHealthInsurance">
-              Life and Health Insurance
-            </option>
-            <option value="Ecommerce">E-commerce</option>
-            <option value="FinancialServices">Financial Services</option>
-            <option value="AdministrationBusiness">
-              Administration Business
-            </option>
-            <option value="Agriculture">Agriculture</option>
-            <option value="Arts">Arts</option>
-            <option value="Educational">Educational</option>
-            <option value="Warehousing">Warehousing</option>
-          </select>
-        </div>
-        <button type="submit" className={styles["button"]}>
-          Search
-        </button>
-      </form>
+    <div className="w-full h-full bg-tertiary-300">
+      <div className={styles["back-button"]}>
+        <img
+          src={backArrow}
+          alt="arrow"
+          className="mr-[15.67px] w-[6px] h-[10px]"
+        />
+        <span>Go Back</span>
+      </div>
+      <div>
+        <img src={circleButton} alt="circle" />
+        <img
+          src={plusIcon}
+          alt="addition"
+          className="absolute top-[102px] left-[14px]"
+        />
+      </div>
+      <div className={styles["form-container"]}>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles["container"]}>
+          <div className={styles["title"]}>Create New Feedback</div>
+          <div className="flex flex-col w-[322px] items-center mb-5 md:w-[400px] lg:w-[500px]">
+            <label className="">Idea</label>
+            <label>Enter Your Amazing Idea!</label>
+            <input
+              {...register("idea")}
+              className={styles["input"]}
+              placeholder="Enter Your Life Changing Idea!"
+            />
+          </div>
+          <div className="flex flex-col w-[322px] items-center mb-5 md:w-[400px] lg:w-[500px]">
+            <textarea
+              {...register("description")}
+              className={clsx(styles["input"], styles["input-description"])}
+              placeholder="Enter Your Life Changing Idea!"
+            />
+          </div>
+          <div className="flex flex-col items-center w-[322px] mt-5 md:w-[400px] lg:w-[500px]"></div>
+          <button type="submit" className={styles["button"]}>
+            Search
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,6 @@ module.exports = {
         "secondary-400": "#F7F8FD",
         "secondary-300": "#3A4374",
         "secondary-200": "#647196",
-        "secondary-100": "#647196",
         "tertiary-500": "#F49F85",
         "tertiary-400": "#62BCFA",
         "tertiary-300": "#F2F2F2",
