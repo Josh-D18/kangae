1ef1c9f7dd07c036d6178228ce9b639f2deb0e0c
Josh D <josh.date@techguilds.com>
2022-10-12 17:32:00 -0400

    added in background color and back button, plus edit button

--- src/components/IdeaDetail/IdeaDetail.tsx
+++ src/components/IdeaDetail/IdeaDetail.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import styles from "./IdeaDetail.module.css";
+import backarrow from "../../assets/icons/grey-back-arrow.png";
 
 const IdeaDetail = () => {
-  return <div>IdeaDetail</div>;
+  return (
+    <div className="h-full bg-tertiary-300">
+      <div className="flex justify-between p-[24px]">
+        <div className="flex items-center mb-[3px] md:pb-[4px]">
+          <img
+            src={backarrow}
+            alt="back arrow"
+            className={styles["back-arrow"]}
+          />
+          <span className={styles["back-link-text"]}>Go Back</span>
+        </div>
+        <button>Edit Idea</button>
+      </div>
+    </div>
+  );
 };
 
 export default IdeaDetail;
