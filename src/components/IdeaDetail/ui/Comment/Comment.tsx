11777135e63903d18bc52b3b43b083e7cc801994
Josh D <josh.date@techguilds.com>
2022-10-16 15:56:41 -0400

    added line for comments

--- src/components/IdeaDetail/ui/Comment/Comment.tsx
+++ src/components/IdeaDetail/ui/Comment/Comment.tsx
@@ -1,30 +1,36 @@
 import profileicon from "../../../../assets/icons/profile-icon.png";
 import styles from "./Comment.module.css";
 
-interface IComment {}
+interface IComment {
+  id: number;
+  comment: string | undefined;
+  fullName: string | undefined;
+  username: string;
+  replies: { username: string; fullname: string; comment: string }[];
+}
 
-const Comment = () => {
+const Comment = (props: IComment) => {
+  const { id, comment, fullName, username, replies } = props;
+
   return (
     <div>
-      <div className="flex">
-        <img
-          src={profileicon}
-          alt="profile"
-          className="max-w-[40px] max-h-[40px]"
-        />
-        <div className="ml-[16px] mr-[80px]">
-          <h3 className={styles["comment-user-fullname"]}>Josh Date</h3>
-          <p className={styles["comment-username"]}>@user1</p>
-        </div>
+      <div>
+        <div className="flex">
+          <img
+            src={profileicon}
+            alt="profile"
+            className="max-w-[40px] max-h-[40px]"
+          />
+          <div className="ml-[16px] mr-[80px]">
+            <h3 className={styles["comment-user-fullname"]}>{fullName}</h3>
+            <p className={styles["comment-username"]}>@{username}</p>
+          </div>
 
-        <button className={styles["comment-reply-button"]}>Reply</button>
+          <button className={styles["comment-reply-button"]}>Reply</button>
+        </div>
+        <p className={styles["comment-description"]}>{comment}</p>
+        <div className={styles["line"]} />
       </div>
-      <p className={styles["comment-description"]}>
-        Also, please allow styles to be applied based on system preferences. I
-        would love to be able to browse Frontend Mentor in the evening after my
-        device’s dark mode turns on without the bright background it currently
-        has.
-      </p>
     </div>
   );
 };
