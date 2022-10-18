2c52a2b7b5774f1b06404a06a936564427741868
Josh D <josh.date@techguilds.com>
2022-10-18 19:15:39 -0400

    added line for replies

--- src/components/IdeaDetail/ui/Comment/Comment.tsx
+++ src/components/IdeaDetail/ui/Comment/Comment.tsx
@@ -8,11 +8,13 @@ interface IComment {
   username: string;
   replies:
     | {
+        id: number;
         username: string;
         fullName: string;
         comment: string;
       }[]
     | {
+        id: number;
         username: string;
         fullName: string;
         comment: string;
@@ -41,6 +43,41 @@ const Comment = (props: IComment) => {
         </div>
         <p className={styles["comment-description"]}>{comment}</p>
         <div className={styles["line"]} />
+      </div>
+      <div className={styles["comment-replies"]}>
+        <div className={styles["line-reply"]} />
+        <div className="relative bottom-[281px]">
+          {replies?.map(({ id, username, fullName, comment }) => (
+            <div key={id}>
+              <div className="ml-[23px] mr-[24px]">
+                <div className="flex">
+                  <img
+                    src={profileicon}
+                    alt="profile"
+                    className="max-w-[40px] max-h-[40px]"
+                  />
+                  <div className="ml-[16px] mr-[80px]">
+                    <h3 className={styles["comment-user-fullname"]}>
+                      {fullName}
+                    </h3>
+                    <p className={styles["comment-username"]}>@{username}</p>
+                  </div>
+
+                  <button className={styles["comment-reply-button"]}>
+                    Reply
+                  </button>
+                </div>
+                <p className={styles["comment-description"]}>
+                  <span className="font-primary font-[700] text-[13px] leading-[18.79px] text-primary-600">
+                    @{username}
+                  </span>
+                  &nbsp;
+                  {comment}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/data/comments.ts
+++ src/data/comments.ts
@@ -8,12 +8,14 @@ export const comments = [
       "Also, please allow styles to be applied based on system preferences. I would love to be able to browse Frontend Mentor in the evening after my device’s dark mode turns on without the bright background it currently has.",
     replies: [
       {
+        id: 8,
         username: "user3",
         fullName: "Ryan Welles",
         comment:
           "While waiting for dark mode, there are browser extensions that will also do the job. Search for 'dark theme” followed by your browser. There might be a need to turn off the extension for sites with naturally black backgrounds though.",
       },
       {
+        id: 9,
         username: "user3",
         fullName: "Ryan Welles",
         comment:
@@ -22,7 +24,7 @@ export const comments = [
     ],
   },
   {
-    id: 2,
+    id: 10,
     username: "user2",
     fullName: "Josh Date",
     online: true,
@@ -30,12 +32,14 @@ export const comments = [
       "Also, please allow styles to be applied based on system preferences. I would love to be able to browse Frontend Mentor in the evening after my device’s dark mode turns on without the bright background it currently has.",
     replies: [
       {
+        id: 43,
         username: "user3",
         fullName: "Ryan Welles",
         comment:
           "While waiting for dark mode, there are browser extensions that will also do the job. Search for 'dark theme” followed by your browser. There might be a need to turn off the extension for sites with naturally black backgrounds though.",
       },
       {
+        id: 33,
         username: "user3",
         fullName: "Ryan Welles",
         comment:
@@ -44,7 +48,7 @@ export const comments = [
     ],
   },
   {
-    id: 3,
+    id: 43,
     username: "user3",
     fullName: "Josh Date",
     online: true,
@@ -52,13 +56,15 @@ export const comments = [
       "Also, please allow styles to be applied based on system preferences. I would love to be able to browse Frontend Mentor in the evening after my device’s dark mode turns on without the bright background it currently has.",
     replies: [
       {
-        username: "user3",
+        id: 73,
+        username: "user5",
         fullName: "Ryan Welles",
         comment:
           "While waiting for dark mode, there are browser extensions that will also do the job. Search for 'dark theme” followed by your browser. There might be a need to turn off the extension for sites with naturally black backgrounds though.",
       },
       {
-        username: "user3",
+        id: 83,
+        username: "user7",
         fullName: "Ryan Welles",
         comment:
           "Good point! Using any kind of style extension is great and can be highly customizable, like the ability to change contrast and brightness. I'd prefer not to use one of such extensions, however, for security and privacy reasons.",
@@ -75,12 +81,14 @@ export const comments = [
       "Also, please allow styles to be applied based on system preferences. I would love to be able to browse Frontend Mentor in the evening after my device’s dark mode turns on without the bright background it currently has.",
     replies: [
       {
+        id: 37,
         username: "user3",
         fullName: "Ryan Welles",
         comment:
           "While waiting for dark mode, there are browser extensions that will also do the job. Search for 'dark theme” followed by your browser. There might be a need to turn off the extension for sites with naturally black backgrounds though.",
       },
       {
+        id: 36,
         username: "user3",
         fullName: "Ryan Welles",
         comment:
